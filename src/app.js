1d8dd6ef02fa9181b711ca5c98bb012d0ec39e3f
Anthony Alvarado <anthonydalvarado@gmail.com>
2023-10-30 15:10:27 -0400

    done

---
+++ src/app.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  document.querySelector("#btn").addEventListener("click", () => {
+    const excuse = generateExcuse();
+    document.querySelector("#excuse").textContent = excuse;
+  });
+  const initialExcuse = generateExcuse();
+  document.querySelector("#excuse").textContent = initialExcuse;
+  console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["student", "donkey", "ogre", "driver", "soldier", "dragon"];
+  let action = ["took my", "threw my", "ate my", "stole my", "bit my"];
+  let possession = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] + 
+    " " +
+    possession[possessionIndex] +
+    " " +
+    where[whereIndex]
+  );
+};
